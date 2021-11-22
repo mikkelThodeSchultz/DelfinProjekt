bb21c437529fe6e50060f2b9913fc1824448a3d2
mikkelThodeSchultz <mikk75c4@stud.kea.dk>
2021-11-22 11:43:57 +0100

    Testing swimmer status

--- src/com/company/Member.java
+++ src/com/company/Member.java
@@ -9,22 +9,32 @@ public class Member {
     private String email;
     private String homeAddress;
     private LocalDate birthdate;
+    private boolean hasSeniorDiscount;
+    private boolean isSeniorSwimmer;
 
-    public Member(String name, String phoneNumber, String email, String homeAddress, int day, int month, int year){
+    public Member(String name, String phoneNumber, String email, String homeAddress, int day, int month, int year) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.homeAddress = homeAddress;
-        this.birthdate = LocalDate.of(year,month,day);
-
+        this.birthdate = LocalDate.of(year, month, day);
     }
 
-    public int calculateAge(){
+    public int calculateAge() {
         LocalDate currentDate = LocalDate.now();
-        if (birthdate!=null) {
+        if (birthdate != null) {
             return Period.between(birthdate, currentDate).getYears();
+        } else return -1;
+    }
+    //TODO skal være i formand klassen.
+    public void createNewMember() {
+        Member member = new Member("Torben", "12345678", "Torbensmail@mail.com", "torbensvej 31", 24, 12, 2000);
+        if (member.calculateAge() >= 60) {
+            hasSeniorDiscount = true;
         }
-        else return -1;
+        if (member.calculateAge() >= 18) {
+            isSeniorSwimmer = true;
+        }
     }
 
     public String getName() {
