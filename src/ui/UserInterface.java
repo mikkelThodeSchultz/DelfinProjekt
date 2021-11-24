6695033675c2c69c20271d0de33b0f01472bf05e
mms <mark425e@stud.kea.dk>
2021-11-24 10:57:13 +0100

    Changes by Mikkel Thode Schultz

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -8,4 +8,61 @@ import java.util.Scanner;
 
 public class UserInterface {
     Scanner sc = new Scanner(System.in);
+
+
+    public String userInputString(){
+        return sc.nextLine();
+    }
+    
+    public void fuckStartController(){
+        Controller controller = new Controller();
+    }
+
+    public void printMemberLists(){
+        List members = controller.memberList();
+        System.out.println(members);
+    }
+
+    public void getWelcomeMessage(){
+        System.out.println("""
+                Velkommen til Delfinen.
+                Den største svømmeklub i Nørrum Snede
+                """);
+
+    }
+
+    public void getMainMenu(){
+        System.out.println("""
+                Tryk på den tast der svarer til det menupunkt du ønsker at vælge.
+                
+                1 - Medlemmer
+                2 - Kontingenter
+                3 - Konkurrencer
+                0 - Luk programmet
+            
+                """);
+    }
+
+    public void getMemberMenu(){
+        System.out.println("""
+                Tryk på den tast der svarer til det menupunkt du ønsker at vælge.
+                
+                1 - Find medlem
+                2 - Opret medlem
+                0 - Tilbage til hovedmenu
+                
+                """);
+    }
+
+    public void getFindMemberMenu(){
+        //TODO redigering i et medlems info bør også afføde ændring i medlemsnummer
+        System.out.println("""
+                1 - Rediger navn
+                2 - Rediger adresse
+                3 - Rediger telefonnummer
+                4 - Rediger fødselsdato
+                
+                """);
+    }
 }
+
